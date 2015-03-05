928e79a4e9e3486139d9d49a82b0c246240058b6
matt-hammond-bbc <matt.hammond@bbc.co.uk>
2015-03-05 12:14:49 +0000

    Added way to record wall clock dispersions in a client.
    Requires pydvbcss master branch on GitHub as of commit:
    6d9e432242d5199cb4d3f8c71af5a249c8f8a871
    (or later)

---
+++ src/dispersion.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+#
+# Copyright 2015 British Broadcasting Corporation
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class DispersionRecorder(object):
+
+    def __init__(self, dispersionAlgorithm):
+        """\
+        :param dispersionAlgorithm: The algorithm object to obtain dispersions from.
+        
+        The algorithm object must have an onClockAdjusted method that can be overriden or replaced
+        with the same arguments as the one defined for :class:`~dvbcss.protocol.client.wc.algorithm.LowestDispersionCandidate`.
+        
+        Works by replacing the onClockAdjusted method in the algorithm object,
+        so beware if using other code that tries to do the same.
+        
+        Usage:
+        
+        .. code-block:: python
+        
+            algorithm = LowestDispersionAlgorithm(...)
+            
+            ... create a wall clock client that uses this algorithm object ...
+        
+            recorder = DispersionRecorder(algorithm)
+            
+            ...
+            
+            recorder.start()
+            
+            ...
+            
+            recorder.stop()   # not necessary, but will stop memory being filled!
+            
+            
+            t=int(raw_input("Enter a wall clock time:"))
+            disp = recorder.dispersionAt(t)
+            print "At wall clock time "+str(t)+", the dispersion was:",disp
+            
+        """
+        super(DispersionRecorder,self).__init__()
+        self.changeHistory = []
+        self.recording = False
+        self.algorithm = dispersionAlgorithm
+        
+        # plug into the algorithm object to receive the onClockAdjusted calls
+        self.original_onClockAdjusted = self.algorithm.onClockAdjusted
+        self.algorithm.onClockAdjusted = self.onClockAdjusted
+        
+        
+    def onClockAdjusted(self, timeAfterAdjustment, adjustment, oldDispersionNanos, newDispersionNanos, dispersionGrowthRate):
+        if self.recording:
+            entry = timeAfterAdjustment, adjustment, oldDispersionNanos, newDispersionNanos, dispersionGrowthRate
+            self.changeHistory.append(entry)
+            
+        self.original_onClockAdjusted(timeAfterAdjustment, adjustment, oldDispersionNanos, newDispersionNanos, dispersionGrowthRate)
+            
+            
+    def clear(self):
+        """\
+        Clear the recorded history.
+        """
+        self.changeHistory = []
+        
+        
+    def start(self):
+        """\
+        Start recording changes in dispersion.
+        
+        If already recording, then this method call does nothing.
+        """
+        self.recording = True
+        
+        
+    def stop(self):
+        """\
+        Stop recording changes in dispersion.
+        
+        If already not recording, then this method call does nothing.
+        """
+        self.recording = False
+    
+    
+    def dispersionAt(self, wcTime):
+        """\
+        Calculate the dispersion at a given wall clock time, using the recorded history.
+        
+        :param wcTime: time of the wall clock
+        :returns: dispersion (in nanoseconds) when the wall clock had the time specified
+        """
+        
+        changeInfo = None
+        for ci in self.changeHistory:
+            when = ci[0]
+            if when <= wcTime:
+                changeInfo = ci
+            else:
+                pass # don't abort immediately but instead
+                # keep looking through because, due to clock adjustment we
+                # might get a later recorded history entry that covers the
+                # same range of wall clock values (because the clock could jump
+                # backwards when adjusted)
+        
+        if changeInfo is None:
+            raise ValueError("History did not contain any entries early enough to give dispersion at time "+str(wcTime))
+        
+        # unpack    
+        when, adjustment, oldDispersionNanos, newDispersionNanos, dispersionGrowthRate = changeInfo
+        
+        # 'when' is before 'wcTime'
+        # so we extrapolate the newDispersion
+        timeDiff = wcTime - when
+        dispersion = newDispersionNanos + dispersionGrowthRate * timeDiff
+        
+        return dispersion
+
+
---
+++ tests/test_dispersion.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+#
+# Copyright 2015 British Broadcasting Corporation
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Unit-tests for dispersion history recorder code
+"""
+
+import sys
+sys.path.append("../src")
+
+
+from dispersion import DispersionRecorder
+
+
+import unittest
+
+
+class Mock_Algorithm(object):
+
+    def onClockAdjusted(self, timeAfterAdjustment, adjustment, oldDispersionNanos, newDispersionNanos, dispersionGrowthRate):
+        pass
+        
+
+class Test_DispersionRecorder(unittest.TestCase):
+
+    def test_plugsInCorrectly(self):
+    
+        algorithm = Mock_Algorithm()
+        oldHookValue = algorithm.onClockAdjusted
+        
+        recorder = DispersionRecorder(algorithm)
+        self.assertNotEquals(oldHookValue, algorithm.onClockAdjusted)
+    
+    
+    def test_notInitiallyRecording(self):
+    
+        algorithm = Mock_Algorithm()
+        recorder = DispersionRecorder(algorithm)
+
+        algorithm.onClockAdjusted( 1000, 0, 0, 100, 1 )
+            
+        self.assertRaises(ValueError, recorder.dispersionAt, 1001)
+
+
+    def test_recordsSimple(self):
+
+        algorithm = Mock_Algorithm()
+        recorder = DispersionRecorder(algorithm)
+        recorder.start()
+
+        algorithm.onClockAdjusted( 1000, 0, 0, 100, 2 )
+
+        self.assertEquals(102, recorder.dispersionAt(1001))
+
+
+    def test_errorIfToEarly(self):
+
+        algorithm = Mock_Algorithm()
+        recorder = DispersionRecorder(algorithm)
+        recorder.start()
+
+        algorithm.onClockAdjusted( 1000, 0, 0, 100, 2 )
+            
+        self.assertRaises(ValueError, recorder.dispersionAt, 999)
+
+
+    def test_multipleHistoryEntries(self):
+
+        algorithm = Mock_Algorithm()
+        recorder = DispersionRecorder(algorithm)
+        recorder.start()
+
+        algorithm.onClockAdjusted( 1000, 0,     0, 100, 2 )
+        algorithm.onClockAdjusted( 2000, 3,  1994, 110, 2 )
+        algorithm.onClockAdjusted( 3000, -2, 2114,  90, 3 )
+
+        self.assertEquals( 100+  2, recorder.dispersionAt(1001))
+        self.assertEquals( 100+100, recorder.dispersionAt(1050))
+        self.assertEquals( 110+ 12, recorder.dispersionAt(2006))
+        self.assertEquals(  90+  9, recorder.dispersionAt(3003))
+
+
+if __name__ == "__main__":
+
+    unittest.main()
